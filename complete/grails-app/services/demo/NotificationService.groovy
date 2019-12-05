3b64235ad0be5ce9e95f6d76d37a00ed4e52d32d
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-05 07:05:21 +0100

    remove CompileStatic from interfaces

--- complete/grails-app/services/demo/NotificationService.groovy
+++ complete/grails-app/services/demo/NotificationService.groovy
@@ -1,10 +1,8 @@
 package demo
 
 import grails.gorm.services.Service
-import groovy.transform.CompileStatic
 
 @Service(Notification)
-@CompileStatic
 interface NotificationService {
     int count()
     void deleteByEmail(String email)
--- complete/grails-app/services/demo/UserService.groovy
+++ complete/grails-app/services/demo/UserService.groovy
@@ -1,10 +1,8 @@
 package demo
 
 import grails.gorm.services.Service
-import groovy.transform.CompileStatic
 
 @Service(User)
-@CompileStatic
 interface UserService {
     int count()
     void deleteByEmail(String email)
